5cc316206288fbd420c8471c0d64e88453b489fe
kestvir <v.kestutis123@gmail.com>
2020-08-22 22:00:35 +0300

    ...

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 
 from rest_framework import serializers
-import django.contrib.auth.password_validation as validators
+from django.contrib.auth.password_validation import validate_password
+
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
